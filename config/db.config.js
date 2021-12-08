3fbd2d65b156e3f467cfd0d3f0bc992e66460b1b
Omar Chong <al221811717@gmail.com>
2021-12-08 16:39:54 -0600

    Acessos base modificados

--- config/db.config.js
+++ config/db.config.js
@@ -4,7 +4,7 @@ const dbConn = mysql.createConnection({
     host: "database-1.cd0ithle1pfb.us-east-1.rds.amazonaws.com",
 	user: "admin",
 	password: "xvP5&$cr0R3o",
-	database: "innodb",
+	database: "gpds",
 });
 
 dbConn.connect(function(error){
